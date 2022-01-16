0215f63905c5b48e65c56179bb4e59397caa5df0
Nathan Reed <vemhira@gmail.com>
2022-01-15 22:26:32 -0600

    format changes

--- smlibppm/core/circle.py
+++ smlibppm/core/circle.py
@@ -1,8 +1,17 @@
 from smlibppm.core.pixel import put_pixel
 
-# yikes this solution is weird thanks to / 
+
+# yikes this solution is weird thanks to /
 # https://stackoverflow.com/questions/22777049/how-can-i-draw-a-circle-in-a-data-array-map-in-python
-def draw_circle(color: list, pixels: list, width: int, height: int, center: tuple, radius: int, elispon: float):
+def draw_circle(
+    color: list,
+    pixels: list,
+    width: int,
+    height: int,
+    center: tuple,
+    radius: int,
+    elispon: float,
+):
     """[draw a circle]
 
     Args:
@@ -16,5 +25,8 @@ def draw_circle(color: list, pixels: list, width: int, height: int, center: tupl
     """
     for y in range(height):
         for x in range(width):
-            if abs((x-center[0])**2 + (y-center[1])**2 - radius**2) < elispon**2:
-                put_pixel(color, pixels, width, (x,y))
+            if (
+                abs((x - center[0]) ** 2 + (y - center[1]) ** 2 - radius ** 2)
+                < elispon ** 2
+            ):
+                put_pixel(color, pixels, width, (x, y))
